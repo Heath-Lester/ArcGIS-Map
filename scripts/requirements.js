31691643c4884b63b4bcafabbef4265a97953b3a
Heath Lester <h.allen.lester@gmail.com>
2024-03-19 01:14:26 -0400

    add scale and legend

--- scripts/requirements.js
+++ scripts/requirements.js
@@ -1,10 +1,23 @@
 export const requirements = () => {
-	return require(["esri/config", "esri/Map", "esri/views/MapView"], function (esriConfig, Map, MapView) {
+	return require([
+		"esri/config",
+		"esri/Map",
+		"esri/WebMap",
+		"esri/views/MapView",
+		"esri/widgets/ScaleBar",
+		"esri/widgets/Legend",
+	], function (esriConfig, Map, WebMap, MapView, ScaleBar, Legend) {
 		esriConfig.apiKey = "get-your-own-key-ya-mooch";
 
-		const map = new Map({
-			basemap: "arcgis/topographic",
+		const map = new WebMap({
+			portalItem: {
+				id: "6dfb41e6b79b4dcd820c79717e465eac",
+			},
 		});
+
+		// const map = new Map({
+		// 	basemap: "arcgis/topographic",
+		// });
 
 		const view = new MapView({
 			map: map,
@@ -12,5 +25,16 @@ export const requirements = () => {
 			zoom: 13,
 			container: "viewDiv",
 		});
+
+		const scalebar = new ScaleBar({
+			view: view,
+		});
+
+		const legend = new Legend({
+			view: view,
+		});
+
+		view.ui.add(scalebar, "bottom-left");
+		view.ui.add(legend, "top-right");
 	});
 };
